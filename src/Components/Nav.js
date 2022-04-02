23957a93a048e071541333f5aab2f5817639ec78
Will Harris <bharris010486@gmail.com>
2022-04-02 11:27:16 -0400

    more work on the projects section

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,11 +3,12 @@ import RegNav from "./RegNav";
 import MobileNav from "./MobileNav";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import logo from '../assets/logo.png'
 
 const Nav = () => {
   return (
     <motion.div>
-      <Link to= '/'><motion.h1 initial={{y: -300}} animate={{y: 0}} transition={{duration: .5}} className='logo'><span style={{color: "#17252A"}}>w</span>H.</motion.h1></Link>
+      <Link to= '/'><motion.img src={logo} initial={{y: -300}} animate={{y: 0}} transition={{duration: .5}} className='logo' /></Link>
       <RegNav />
       <MobileNav />
     </motion.div>
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -8,7 +8,12 @@ import gifPro from '../assets/gifPro.png'
 import workoutPro from '../assets/workoutPro.png'
 
 const Work = () => {
-  const [graffi, setGraffi] = useState('')
+  const [graffi, setGraffi] = useState(false)
+  const [chore, setChore] = useState(false)
+  const [port, setPort] = useState(false)
+  const [work, setWork] = useState(false)
+  const [gif, setGif] = useState(false)
+  const [poke, setPoke] = useState(false)
 
   return (
     <motion.div>
@@ -21,22 +26,25 @@ const Work = () => {
           className="workBox"
         >
           <div className="projectBox1">
-          <button onMouseEnter={()=> setGraffi(true)} style={{ display: graffi ? 'none' : 'flex'}}>Button</button>
-          <div style={{display: graffi ? 'flex' : 'none'}} onMouseEnter={() => setGraffi(true)} onMouseLeave={() => setGraffi(false)} class="proCard">
+          <button onMouseEnter={()=> setGraffi(true)} style={{ visibility: graffi ? 'hidden' : 'visible', height: '10%', position: 'absolute', top: '50%', left: '25%', transform: 'translate(-50%,-50%)'}}>Graffi</button>
+          <div style={{visibility: graffi ? 'visible' : 'hidden'}} onMouseEnter={() => setGraffi(true)} onMouseLeave={() => setGraffi(false)} class="proCard">
             
-            <motion.img whileHover={{scale: 1.2, transition: {duration: .3}}} alt='project' className='portfolioImage' src={graffiProject} />
+            <motion.img  whileHover={{scale: 1.2, transition: {duration: .3}}} alt='project' className='portfolioImage' src={graffiProject} />
               <h3>Project 1</h3>
               <p>Short description</p>
               
               <p>onMouseEnter drop down for github source code, and live</p>
               </div>
-            <div class="proCard">
+              <button onMouseEnter={()=> setChore(true)} style={{ visibility: chore ? 'hidden' : 'visible', height: '10%', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)'}}>choreMe</button>
+            <div style={{visibility: chore ? 'visible' : 'hidden'}} onMouseEnter={() => setChore(true)} onMouseLeave={() => setChore(false)} class="proCard">
+            
             <motion.img  whileHover={{scale: 1.2, transition: {duration: .3}}} alt='project' className='portfolioImage' src={choreMeProject} />
               <h3>Project 2</h3>
               <p>Short description</p>
               <p>onMouseEnter drop down for github source code, and live</p>
               </div>
-            <div class="proCard">
+              <button onMouseEnter={()=> setPort(true)} style={{ visibility: port ? 'hidden' : 'visible', height: '10%', position: 'absolute', top: '50%', left: '75%', transform: 'translate(-50%,-50%)'}}>Portfolio v.1</button>
+            <div style={{visibility: port ? 'visible' : 'hidden'}} onMouseEnter={() => setPort(true)} onMouseLeave={() => setPort(false)} class="proCard">
               <h3>Project 3</h3>
               <p>Short description</p>
               <motion.img whileHover={{scale: 1.2, transition: {duration: .3}}} alt='project' className='portfolioImage' src={PortfolioImage} />
@@ -53,19 +61,22 @@ const Work = () => {
             </span>
           </h1>
           <div className="projectBox1">
-            <div class="proCard">
+          <button onMouseEnter={()=> setWork(true)} style={{ visibility: work ? 'hidden' : 'visible', height: '10%', position: 'absolute', top: '50%', left: '25%', transform: 'translate(-50%,-50%)'}}>Workout Log</button>
+            <div style={{visibility: work ? 'visible' : 'hidden'}} onMouseEnter={() => setWork(true)} onMouseLeave={() => setWork(false)} class="proCard">
               <h3>Project 1</h3>
               <p>Short description</p>
               <motion.img whileHover={{scale: 1.2, transition: {duration: .3}}} alt='project' className='portfolioImage' src={workoutPro} />
               <p>onMouseEnter drop down for github source code, and live</p>
               </div>
-            <div class="proCard">
+              <button onMouseEnter={()=> setGif(true)} style={{ visibility: gif ? 'hidden' : 'visible', height: '10%', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)'}}>Gif Finder</button>
+            <div style={{visibility: gif ? 'visible' : 'hidden'}} onMouseEnter={() => setGif(true)} onMouseLeave={() => setGif(false)} class="proCard">
               <h3>Project 2</h3>
               <p>Short description</p>
               <motion.img whileHover={{scale: 1.2, transition: {duration: .3}}} alt='project' className='portfolioImage' src={gifPro} />
               <p>onMouseEnter drop down for github source code, and live</p>
               </div>
-            <div class="proCard">
+              <button onMouseEnter={()=> setPoke(true)} style={{ visibility: poke ? 'hidden' : 'visible', height: '10%', position: 'absolute', top: '50%', left: '75%', transform: 'translate(-50%,-50%)'}}>Pokemon Catch</button>
+            <div style={{visibility: poke ? 'visible' : 'hidden'}} onMouseEnter={() => setPoke(true)} onMouseLeave={() => setPoke(false)} class="proCard">
               <h3>Project 3</h3>
               <p>Short description</p>
               <motion.img whileHover={{scale: 1.2, transition: {duration: .3}}} alt='project' className='portfolioImage' src={pokePro} />
